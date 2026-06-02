aa81872730da5e98f3ef2268b9d95b79ab8f92ee
MeoWAlwaysOnline <datsun.ihor@gmail.com>
2026-06-02 13:33:16 +0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,142 @@
+// num 2
+console.log("hello world");
+// num 3
+let name = "mykola";
+let age = 12;
+let favouriteGame = "geometryDash";
+console.log(name, age, favouriteGame);
+// num 4
+let smth;
+let smth2;
+console.log(smth, smth2);
+smth = 12;
+smth2 = 24;
+console.log(smth, smth2);
+// num 5 я не дуже поняв завдання
+// without -
+let cashBack ; 
+// not start with a number
+let ninecircles ;
+// javascript - key word
+let JavaScriptKey ;
+//  type of data
+let boolean = true;
+
+// num 6 
+let myString = "hello world";
+let myNumber = 12;
+let boolean2 = true;
+let myNull = null;
+let empty;
+// num 7
+console.log(typeof(myString), typeof(myNumber), typeof(boolean2), typeof(myNull), typeof(empty));
+// num 8
+const name2 = "mykola";
+const school = 250;
+const computer = "windows";
+const city = "kyiv";
+const counrty2 = "ukraine"
+let age2 = 12;
+let favouriteFood = "pizza";
+let favouriteGame2 = "geometrydash";
+let phone = "android";
+let grade = "sixth";
+// num 9
+let country = "ukraine";
+let capital = "kyiv";
+let numberOfPopulation = 33000000;
+let myOpinion = true;
+// num 10
+let message = "hello world !";
+console.log(message.toUpperCase(), message.length);
+// num 11
+let name3 = "ihor";
+console.log(name3.length, name3.toUpperCase());
+// num 12
+console.log(name.length);
+console.log(country.length);
+console.log(computer.length);
+console.log(favouriteGame.length);
+console.log(favouriteFood.length);
+// num 12 (2)
+console.log(name.toUpperCase());
+console.log(country.toUpperCase());
+console.log(computer.toUpperCase());
+console.log(favouriteGame.toUpperCase());
+console.log(favouriteFood.toUpperCase());
+// num 12 (3)
+console.log(name);
+console.log(country);
+console.log(computer);
+console.log(favouriteGame);
+console.log(favouriteFood);
+// num 12 (4)
+alert(name);
+alert(country);
+alert(computer);
+alert(favouriteGame);
+alert(favouriteFood);
+// num 13
+let message2 = "test";
+console.log(message2);
+alert(message2);
+// num 14
+let productName = "super duper cool item";
+let productPrice = 10000;
+console.log(productName, productPrice);
+// num 15
+alert("hello my name is mykola, im 12 yrs old");
+// num 16
+let userName = prompt("what your name");
+console.log(userName);
+// num 17
+let userGame = prompt("whats your favourite game?");
+let userColor = prompt("whats your favourite color");
+let userFood = prompt("whats your favourite food?");
+console.log(userGame, userColor, userFood);
+// num 18
+let userName2 = prompt("whats your name");
+let userAge = prompt("whats your age");
+let userCountry = prompt("whats your country");
+alert(userName2, userAge, userCountry);
+// num 19
+let userNumber = prompt("pick a number");
+console.log(typeof(userNumber));
+// num 20
+let userAnswer = confirm("are u sure?");
+console.log(userAnswer);
+// num 21
+let userAnswer2 = confirm("Ви точно хочете видалити файл?");
+console.log(userAnswer2);
+console.log(typeof(userAnswer2));
+// num 22
+let userQuestion = confirm("чи любиш піцу");
+let userQuestion2 = confirm("чи любиш спорт");
+let userQuestion3 = confirm("чи любиш програмування");
+console.log(userQuestion, userQuestion2, userQuestion3);
+// num 23
+let userData1 = prompt("write ur name");
+let userData2 = prompt("write ur age");
+let userData3 = prompt("write ur city");
+let userData4 = prompt("write ur favorite game");
+console.log(userData1, userData2, userData3, userData4);
+alert(userData1, userData2, userData3, userData4);
+// num 24
+let pizza = "пепероні";
+let pizzaPrice = 200;
+console.log(pizza, pizzaPrice + "грн");
+pizzaPrice = 250;
+console.log(pizza, pizzaPrice + "грн");
+// num 25
+let first = "smth";
+let second = 20;
+let third = NaN;
+let fourth = undefined;
+let fifth = true;
+let sixth = null;
+console.log(typeof(first), typeof(second), typeof(third), typeof(fourth), typeof(fifth), typeof(sixth));
+// num 26
+let dataUser1 = prompt("ur name");
+let dataUser2 = prompt("ur hobby");
+let dataUser3 = prompt("ur favorite game");
+console.log(dataUser1, dataUser2, dataUser3);

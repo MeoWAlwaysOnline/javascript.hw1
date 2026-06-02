86462f6fd241bc5fbdc5808d6b96df44533f5b7b
MeoWAlwaysOnline <datsun.ihor@gmail.com>
2026-06-02 13:31:50 +0300

    Add files via upload

---
+++ hw1.js
@@ -0,0 +1,27 @@
+// № 1
+let age = 12;
+console.log(age);
+// № 2
+let name = "Ігор";
+console.log(name);
+// № 3
+let isStudent = true;
+console.log(isStudent);
+// № 4
+let myString = "я знаю що я нічого не знаю.";
+console.log(myString);
+// № 5
+let myNumber = 5;
+console.log(myNumber + 10);
+// № 6
+let myNull = null;
+console.log(myNull);
+// № 7
+let userName = prompt("Введіть своє ім’я");
+alert(userName);
+console.log(typeof userName);
+// № 8
+let userAnswer = confirm("Ви дійсно хочете покинути сторінку?");
+// № 9
+alert("Увага! Дія може бути небезпечною.");
+confirm("Ви впевнені, що хочете продовжити?");
